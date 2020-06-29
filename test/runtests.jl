b5cea79854517b57d3b611a53a8f03ce0a1f580f
Milan K <milankloewer@gmx.de>
2020-06-29 23:44:49 +0100

    Lossless und lossy tol specified tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
 using ZfpCompression
 using Test
 
-@testset "Zfp.jl" begin
-    @test 4 == zfp_type_size(1)
+@testset "Lossless 1-4D for all types" begin
+
+    for T in (Float32,Float64,Int32,Int64)
+        # test 1-4D
+        for sizes in [(100,),(100,50),(20,30,40),(20,20,20,20)]
+            A = rand(Float32,sizes...)
+            Ac = zfp_compress(A)
+            Ad = similar(A)
+            zfp_decompress!(Ad,Ac)
+            @test Ad == A
+        end
+    end
+end
+
+@testset "Max abs error is bound in 1-4D for all types" begin
+
+    tol = 1e-3
+
+    for T in (Float32,Float64,Int32,Int64)
+        # test 1-4D
+        for sizes in [(100,),(100,50),(20,30,40),(20,20,20,20)]
+            A = rand(Float32,sizes...)
+            Ac = zfp_compress(A,tol=tol)
+            Ad = similar(A)
+            zfp_decompress!(Ad,Ac,tol=tol)
+            @test maximum(abs.(Ad-A)) <= tol
+        end
+    end
 end
